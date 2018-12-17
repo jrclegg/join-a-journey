954deebd864fe932d8081af5a3b97c08ea3ecfaa
jrclegg <clegg.jonathan@rocketmail.com>
2018-12-17 16:03:11 +0000

    add wildlife image gallery component

---
+++ src/components/ImageGallery.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+import caiman from '../images/caiman.png';
+import elephant from '../images/elephant.png';
+import toucan from '../images/toucan.png';
+import tiger from '../images/tiger.png';
+import gorilla from '../images/gorilla.png';
+import chameleon from '../images/chameleon.png';
+import whiteshark from '../images/whiteshark.png';
+import penguins from '../images/emperorpenguin.png';
+
+
+const Wrapper = styled.div `
+    display: -ms-flexbox; /* IE10 */
+    display: flex;
+    -ms-flex-wrap: wrap; /* IE10 */
+    flex-wrap: wrap;
+    padding: 0 4px;
+`
+const SideColumn = styled.div`
+    -ms-flex: 25%; /* IE10 */
+    flex: 25%;
+    max-width: 25%;
+    padding: 0 4px;
+`
+const MidColumn = styled.div`
+    -ms-flex: 25%; /* IE10 */
+    flex: 25%;
+    max-width: 60%;
+    padding: 0 4px;
+`
+const Image = styled.img`
+    width: 100%;
+`
+
+const ImageGallery = () => {
+    return (
+        <Wrapper>
+            <SideColumn>
+                <Image src={caiman} alt="caiman"/>
+                <Image src={elephant} alt="elephant"/>
+                <Image src={toucan} alt="toucan"/>
+            </SideColumn>
+            <MidColumn>
+                <Image src={tiger} alt="tiger"/>
+                <Image src={gorilla} alt="gorilla"/>
+            </MidColumn>
+            <SideColumn>
+                <Image src={chameleon} alt="chameleon"/>
+                <Image src={whiteshark} alt="great white shark"/>
+                <Image src={penguins} alt="penguins"/>
+            </SideColumn>
+        </Wrapper>
+    )
+}
+
+export default ImageGallery
