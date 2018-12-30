68fc11b956d955ed64e96e970aec1c8aaad84b02
jrclegg <clegg.jonathan@rocketmail.com>
2018-12-30 09:56:42 +0000

    make image component reusable

---
+++ src/components/Image.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Image = styled.img`
+    border: 1px solid #ccc;
+    box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
+    max-width: 100%;
+`
+
+export default Image
--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
+import Grid from '../layouts/Grid'
+import Image from '../components/Image'
 import caiman from '../images/caiman.png';
 import elephant from '../images/elephant.png';
 import toucan from '../images/toucan.png';
@@ -8,35 +9,6 @@ import gorilla from '../images/gorilla.png';
 import chameleon from '../images/chameleon.png';
 import whiteshark from '../images/whiteshark.png';
 import penguins from '../images/emperorpenguin.png';
-
-
-const Grid = styled.div `
-    @media screen and (max-width: 768px){
-        display: grid;
-        grid-template-columns: 180px 180px;
-        grid-gap: 10px;
-        padding: 10px;
-        margin-left: 5px;
-    }
-    @media screen and (min-width: 768px){
-        display: grid;
-        grid-template-columns: 250px 250px 250px;
-        grid-gap: 10px;
-        padding: 10px;
-    }
-    @media screen and (min-width: 1024px){
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
-        grid-gap: 20px;
-        margin-left: 20px;
-        margin-right: 20px;
-    }
-`
-const Image = styled.img`
-    border: 1px solid #ccc;
-    box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
-    max-width: 100%;
-`
 
 const ImageGallery = () => {
     return (
