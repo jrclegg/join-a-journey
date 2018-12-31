3f9ffb14f3a7b1a9d349877624f0bfa218f733f9
jrclegg <clegg.jonathan@rocketmail.com>
2018-12-31 12:39:37 +0000

    remove theme Provider from index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
-
-import theme from './theme/sizes'
 
 class Main extends React.Component {
   render() {
@@ -17,8 +14,6 @@ class Main extends React.Component {
 
 ReactDOM.render((
   <BrowserRouter>
-    <ThemeProvider theme={theme}>
       <Main />
-    </ThemeProvider>
   </BrowserRouter>
 ), document.getElementById('root'))
