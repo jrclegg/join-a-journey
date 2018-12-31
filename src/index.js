92c8fb08962a05fcf8e54d5693bc2acb7f099f69
jrclegg <clegg.jonathan@rocketmail.com>
2018-12-31 12:38:23 +0000

    move Home component into App and export App to index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './theme/sizes'
 
 class Main extends React.Component {
   render() {
     return (
-      <Home/>
+      <App/>
     );
   }
 }
 
 ReactDOM.render((
   <BrowserRouter>
-    <Main />
+    <ThemeProvider theme={theme}>
+      <Main />
+    </ThemeProvider>
   </BrowserRouter>
 ), document.getElementById('root'))
